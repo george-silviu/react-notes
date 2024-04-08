758da6268bcb556cf93788b3aa86fcb92b996810
george-silviu <costache.georgesilviu@yahoo.ro>
2024-04-08 18:15:36 +0300

    uploading project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Button from "@mui/material/Button";
+import { DataGrid } from "@mui/x-data-grid";
 
 function App() {
+  const rows = [
+    { id: 1, col1: "Hello", col2: "World" },
+    { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+    { id: 3, col1: "MUI", col2: "is Amazing" },
+  ];
+
+  const columns = [
+    { field: "col1", headerName: "Column 1", width: 150 },
+    { field: "col2", headerName: "Column 2", width: 150 },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="header">Simple notes app</h1>
+
+      <h2 className="intro-message">Insert your knowledge here</h2>
+
+      <div className="buttons-container">
+        <Button
+          style={{ marginRight: "10px" }}
+          variant="contained"
+          color="success"
         >
-          Learn React
-        </a>
-      </header>
+          Add note
+        </Button>
+
+        <Button variant="contained" color="error">
+          Remove note
+        </Button>
+      </div>
+
+      <div className="data-grid">
+        <DataGrid checkboxSelection={true} rows={rows} columns={columns} />
+      </div>
     </div>
   );
 }
